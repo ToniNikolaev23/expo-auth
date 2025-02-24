1d473d25132ed14197d53e895c9c02d3165b3079
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2025-02-24 22:28:40 +0200

    Create Register page, install react-form-hook, resolver and zod and integrate it

---
+++ app/register.tsx
@@ -0,0 +1,188 @@
+import {
+  ActivityIndicator,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { Controller, useForm } from "react-hook-form";
+import { COLORS } from "@/utils/colors";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { useState } from "react";
+import { useRouter } from "expo-router";
+
+const schema = z.object({
+  name: z.string().optional(),
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 8 characters")
+    .max(32, "Password must be at most 32 characters"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const Page = () => {
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const {
+    control,
+    handleSubmit,
+    trigger,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      name: "Tester",
+      email: "a@a.com",
+      password: "123456",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  };
+  return (
+    <View style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1, justifyContent: "center" }}
+      >
+        <Controller
+          control={control}
+          name="name"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Name (optional)"
+                style={styles.input}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor={COLORS.placeholder}
+              />
+            </View>
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="john@doe.com"
+                autoCapitalize="none"
+                style={styles.input}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor={COLORS.placeholder}
+                keyboardType="email-address"
+              />
+              {errors.email && (
+                <Text style={styles.errorText}>{errors.email.message}</Text>
+              )}
+            </View>
+          )}
+        />
+
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, onBlur, value } }) => (
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Password"
+                secureTextEntry
+                style={styles.input}
+                onChangeText={onChange}
+                onBlur={onBlur}
+                value={value}
+                placeholderTextColor={COLORS.placeholder}
+              />
+              {errors.password && (
+                <Text style={styles.errorText}>{errors.password.message}</Text>
+              )}
+            </View>
+          )}
+        />
+
+        <TouchableOpacity
+          onPress={handleSubmit(onSubmit)}
+          disabled={!!errors.email || !!errors.password}
+          style={[
+            styles.button,
+            !errors.email && !errors.password ? {} : styles.buttonDisabled,
+          ]}
+        >
+          <Text style={styles.buttonText}>Sign Up</Text>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+      {loading && (
+        <View style={styles.loadingOverlay}>
+          <ActivityIndicator size="large" color="#fff" />
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default Page;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+  },
+  inputContainer: {
+    marginVertical: 10,
+  },
+  input: {
+    backgroundColor: COLORS.input,
+    borderWidth: 1,
+    borderColor: COLORS.primary,
+    padding: 10,
+    borderRadius: 4,
+    height: 50,
+    color: "#fff",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: COLORS.primary,
+    padding: 12,
+    borderRadius: 4,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "600",
+  },
+  errorText: {
+    color: "#ff6b6b",
+    fontSize: 12,
+    marginTop: 4,
+    marginLeft: 4,
+  },
+  buttonDisabled: {
+    opacity: 0.5,
+  },
+  loadingOverlay: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "rgba(0,0,0,0.6)",
+    justifyContent: "center",
+    zIndex: 1000,
+  },
+});
